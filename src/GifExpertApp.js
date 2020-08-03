da10a7f918a31461f05c6977f1d1d84958ebcdaf
Ricardo Riquelme S <riquelme.iromax@gmail.com>
2020-08-02 23:49:42 -0400

    First commit

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["One Puch"]);
+  const [categories, setCategories] = useState(["Gadot Wonder Woman"]);
 
   //   const handleAdd = () => {
   //     // setCategories(["Centella", ...categories]);
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,26 +1,23 @@
 import React from "react";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+import { GifGridItem } from "./GifGridItem";
 
 export const GifGrid = ({ category }) => {
-  const getGifs = async () => {
-    const url =
-      "https://api.giphy.com/v1/gifs/search?q=Wonder+Woman+Gadot&limit=10&api_key=SZZLWGgeFiJ8sbZc22Q00spEzQY0l8QQ";
-    const resp = await fetch(url);
-    const { data } = await resp.json();
+  const { data: images, loading } = useFetchGifs(category);
 
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    console.log(gifs);
-  };
-
-  getGifs();
+  //   getGifs();
   return (
-    <div>
-      <h3>{category}</h3>
-    </div>
+    <>
+      <h3 className="animate__animated animate__fadeInRight">{category}</h3>
+
+      {loading && (
+        <p className="animate__animated animate__flash">Loading...</p>
+      )}
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
+    </>
   );
 };
