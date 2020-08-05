85b4ebca10a1b1a246960161b1d3144ee1f84b5a
Ricardo Riquelme <ririquel@alumnos.ubiobio.cl>
2020-08-04 22:44:09 -0400

    Cambio nina agdal

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from "./components/AddCategory";
 import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["Gadot Wonder Woman"]);
+  const [categories, setCategories] = useState(["nina agdal"]);
 
   //   const handleAdd = () => {
   //     // setCategories(["Centella", ...categories]);
